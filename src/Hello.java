086ef10b46ac47d9a2ccb48f6c5e491b527d4052
qweasd235 <kim451212@>
2023-08-05 16:44:59 +0900

    commit 테스트

---
+++ src/Hello.java
@@ -0,0 +1,7 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("이름:최근욱");
+        System.out.println("성별:남성");
+        System.out.println("이메일:235qwerty@gmail.com");
+    }
+}
---
+++ src/Rectangle.java
@@ -0,0 +1,11 @@
+public class Rectangle {
+    public static void main(String[] args) {
+        String rectangle = "**********";
+
+        for (int i = 0; i < 1000; i++){
+            System.out.println(rectangle);
+        }
+
+
+    }
+}
