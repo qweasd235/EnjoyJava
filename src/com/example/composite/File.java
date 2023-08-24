bf05a3b4f5374d6363548c802df5efdf5de3f2a2
qweasd235 <235qwerty@naver.com>
2023-08-24 19:57:16 +0900

    자바공부 8/24 - composite 패턴 예제 공부

---
+++ src/com/example/composite/File.java
@@ -0,0 +1,20 @@
+package com.example.composite;
+
+public class File extends Node{
+    private long size;
+
+    public File(String name, long size){
+        super(name);
+        this.size = size;
+    }
+
+    @Override
+    public long getSize() {
+        return this.size;
+    }
+
+    @Override
+    public boolean isFolder() {
+        return false;
+    }
+}
